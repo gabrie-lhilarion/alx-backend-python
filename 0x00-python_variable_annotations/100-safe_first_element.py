f7e9533b889b63bd699876785a407b7bfd570919
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-07-05 04:52:58 +0100

    Safety first

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,47 @@
 #!/usr/bin/env python3
+
+"""
+Module: safe_first_element
+
+This module provides a function to safely retrieve the
+first element of a sequence.
+If the sequence is empty, the function returns None.
+
+Function:
+    safe_first_element(lst): Returns the first element
+    of the sequence or None.
+
+Example:
+    >>> safe_first_element([1, 2, 3])
+    1
+    >>> safe_first_element([])
+    None
+"""
+
+from typing import Sequence, Any, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+    Returns the first element of the sequence if it exists,
+    otherwise returns None.
+
+    Args:
+        lst (Sequence[Any]): The sequence to retrieve the
+        first element from.
+
+    Returns:
+        Optional[Any]: The first element of the sequence,
+        or None if the sequence is empty.
+
+    Example:
+        >>> safe_first_element([1, 2, 3])
+        1
+        >>> safe_first_element([])
+        None
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
+
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,55 @@
 #!/usr/bin/env python3
+
+"""
+Module: safely_get_value
+
+This module provides a function to safely retrieve a value from a dictionary.
+If the key does not exist in the dictionary, the function returns a default value.
+
+Function:
+    safely_get_value(dct, key, default): Returns the value associated with the key or the default value.
+
+Example:
+    >>> my_dict = {'a': 1, 'b': 2}
+    >>> safely_get_value(my_dict, 'a')
+    1
+    >>> safely_get_value(my_dict, 'c', 3)
+    3
+"""
+
+from typing import TypeVar, Mapping, Any, Optional
+
+# Define a TypeVar for the return value
+T = TypeVar('T')
+
+
+def safely_get_value(
+        dct: Mapping[Any, T], key: Any, default: Optional[T] = None
+        ) -> Optional[T]:
+    """
+    Returns the value associated with the given key
+    in the dictionary.  If the key does not exist,
+    returns the default value.
+
+    Args:
+        dct (Mapping[Any, T]): The dictionary to search.
+        key (Any): The key to look for in the dictionary.
+        default (Optional[T], optional): The default value 
+        to return if the key is not found. Defaults to None.
+
+    Returns:
+        Optional[T]: The value associated with the key, 
+        or the default value.
+
+    Example:
+        >>> my_dict = {'a': 1, 'b': 2}
+        >>> safely_get_value(my_dict, 'a')
+        1
+        >>> safely_get_value(my_dict, 'c', 3)
+        3
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+
