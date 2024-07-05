6b9a6da81eb82053f5b4f9c5d77557be13499343
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-07-05 04:58:40 +0100

    Type checking correction

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
 
+"""
+Module: zoom_array
+
+This module provides a function to zoom in on
+elements of a tuple.
+The function returns a list where each item
+in the input tuple is repeated
+a specified number of times.
+
+Function:
+    zoom_array(lst, factor): Returns a list with each item in the input tuple
+                             repeated 'factor' times.
+
+Example:
+    >>> array = (12, 72, 91)
+    >>> zoom_2x = zoom_array(array)
+    >>> zoom_2x
+    [12, 12, 72, 72, 91, 91]
+    >>> zoom_3x = zoom_array(array, 3)
+    >>> zoom_3x
+    [12, 12, 12, 72, 72, 72, 91, 91, 91]
+"""
+
 from typing import List, Tuple
 
 
